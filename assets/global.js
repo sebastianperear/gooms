4545fd3147f6f8e308469f58efefc822f33d4126
Sebastian Perea <sebasperea1@gmail.com>
2023-02-07 21:02:24 -0500

    Back

--- assets/global.js
+++ assets/global.js
@@ -157,99 +157,99 @@ class QuantityInput extends HTMLElement {
 }
 
 // Custom 
-class QuantityRecomendationInput extends HTMLElement {
-  constructor() {
-    super();
+// class QuantityRecomendationInput extends HTMLElement {
+//   constructor() {
+//     super();
     
     
-    this.lineItemStatusElement = document.getElementById('shopping-cart-line-item-status') || document.getElementById('CartDrawer-LineItemStatus');
+//     this.lineItemStatusElement = document.getElementById('shopping-cart-line-item-status') || document.getElementById('CartDrawer-LineItemStatus');
 
-    this.currentItemCount = Array.from(this.querySelectorAll('[name="updates[]"]'))
-      .reduce((total, quantityInput) => total + parseInt(quantityInput.value), 0);
-      this.cart = document.querySelector('cart-notification') || document.querySelector('cart-drawer');
-    this.debouncedOnChange = debounce((event) => {
-      this.onChange(event);
-    }, 300);
+//     this.currentItemCount = Array.from(this.querySelectorAll('[name="updates[]"]'))
+//       .reduce((total, quantityInput) => total + parseInt(quantityInput.value), 0);
+//       this.cart = document.querySelector('cart-notification') || document.querySelector('cart-drawer');
+//     this.debouncedOnChange = debounce((event) => {
+//       this.onChange(event);
+//     }, 300);
 
-    this.addEventListener('change', this.debouncedOnChange.bind(this));
+//     this.addEventListener('change', this.debouncedOnChange.bind(this));
     
-  }
+//   }
 
-  onChange(event) {
-    this.updateQuantity(event.target.dataset.index, event.target.value, document.activeElement.getAttribute('name'));
-  }
+//   onChange(event) {
+//     this.updateQuantity(event.target.dataset.index, event.target.value, document.activeElement.getAttribute('name'));
+//   }
 
-  getSectionsToRender() {
-    return [
-      {
-        id: 'quantity-items',
-        section: document.getElementById('quantity-items').dataset.id,
-        selector: '.js-contents',
-      }
-    ];
-  }
+//   getSectionsToRender() {
+//     return [
+//       {
+//         id: 'quantity-items',
+//         section: document.getElementById('quantity-items').dataset.id,
+//         selector: '.js-contents',
+//       }
+//     ];
+//   }
 
-  updateQuantity(line, quantity, name) {
-    this.enableLoading(line);
+//   updateQuantity(line, quantity, name) {
+//     this.enableLoading(line);
     
-    const body = JSON.stringify({
-      line,
-      quantity,
-      sections: this.getSectionsToRender().map((section) => section.section),
-      sections_url: window.location.pathname
-    });
+//     const body = JSON.stringify({
+//       line,
+//       quantity,
+//       sections: this.getSectionsToRender().map((section) => section.section),
+//       sections_url: window.location.pathname
+//     });
 
-    fetch(`${routes.cart_change_url}`, {...fetchConfig(), ...{ body }})
-      .then((response) => {
-        return response.text();
+//     fetch(`${routes.cart_change_url}`, {...fetchConfig(), ...{ body }})
+//       .then((response) => {
+//         return response.text();
         
-      })
-      .then((state) => {
+//       })
+//       .then((state) => {
         
-        const parsedState = JSON.parse(state);
-        this.classList.toggle('is-empty', parsedState.item_count === 0);
-        window.location.reload()
-        this.getSectionsToRender().forEach((section => {
-          const elementToReplace =
-            document.getElementById(section.id).querySelector(section.selector) || document.getElementById(section.id);
-          elementToReplace.innerHTML =
-            this.getSectionInnerHTML(parsedState.sections[section.section], section.selector);
-        }));
+//         const parsedState = JSON.parse(state);
+//         this.classList.toggle('is-empty', parsedState.item_count === 0);
+//         window.location.reload()
+//         this.getSectionsToRender().forEach((section => {
+//           const elementToReplace =
+//             document.getElementById(section.id).querySelector(section.selector) || document.getElementById(section.id);
+//           elementToReplace.innerHTML =
+//             this.getSectionInnerHTML(parsedState.sections[section.section], section.selector);
+//         }));
         
-        this.updateLiveRegions(line, parsedState.item_count);
+//         this.updateLiveRegions(line, parsedState.item_count);
 
 
         
-      }).catch(() => {
+//       }).catch(() => {
         
-        this.querySelectorAll('.loading-overlay').forEach((overlay) => overlay.classList.add('hidden'));
-        const errors = document.getElementById('cart-errors') || document.getElementById('CartDrawer-CartErrors');
-        this.disableLoading();
-      });
+//         this.querySelectorAll('.loading-overlay').forEach((overlay) => overlay.classList.add('hidden'));
+//         const errors = document.getElementById('cart-errors') || document.getElementById('CartDrawer-CartErrors');
+//         this.disableLoading();
+//       });
 
     
 
-  }
+//   }
 
 
 
-  enableLoading(line) {
-    const mainCartItems = document.getElementById('quantity-items');
-    mainCartItems.classList.add('cart__items--disabled');
+//   enableLoading(line) {
+//     const mainCartItems = document.getElementById('quantity-items');
+//     mainCartItems.classList.add('cart__items--disabled');
 
-    const cartItemElements = this.querySelectorAll(`#CartItem-${line} .loading-overlay`);
-    const cartDrawerItemElements = this.querySelectorAll(`#CartDrawer-Item-${line} .loading-overlay`);
+//     const cartItemElements = this.querySelectorAll(`#CartItem-${line} .loading-overlay`);
+//     const cartDrawerItemElements = this.querySelectorAll(`#CartDrawer-Item-${line} .loading-overlay`);
 
-    [...cartItemElements, ...cartDrawerItemElements].forEach((overlay) => overlay.classList.remove('hidden'));
+//     [...cartItemElements, ...cartDrawerItemElements].forEach((overlay) => overlay.classList.remove('hidden'));
 
-    document.activeElement.blur();
-  }
+//     document.activeElement.blur();
+//   }
 
-  disableLoading() {
-    const mainCartItems = document.getElementById('quantity-items');
-    mainCartItems.classList.remove('cart__items--disabled');
-  }
-}
+//   disableLoading() {
+//     const mainCartItems = document.getElementById('quantity-items');
+//     mainCartItems.classList.remove('cart__items--disabled');
+//   }
+// }
 
 // Custom ----
 customElements.define('quantity-input', QuantityInput);
