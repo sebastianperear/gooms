8d522f3c371db39294d230f659514897b47a70ef
Sebastian Perea <sebasperea1@gmail.com>
2023-03-27 20:43:32 -0500

    Add to prod

--- assets/custom.js
+++ assets/custom.js
@@ -1,6 +1,8 @@
-function btnClick(event) {
-    
-    alert('Se ha dado clic al botón!');
-    event.preventDefault();
+function step_1(event) {
+    const step_1_section = document.querySelector(".step_1");
+    step_1_section.style.display = "none";
+    event.preventDefault()
     
   }
+const step_1_btn = document.querySelector(".button_image-text");
+step_1_btn.addEventListener("click",step_1, false);
